b1b0c9df58429fa0f5865c88be82437008673cea
Victor Hugo Barros <hey.zooi@gmail.com>
2018-11-23 16:36:10 -0800

    first commit

---
+++ KinveySnippets/CodeSnippet.cs
@@ -0,0 +1,78 @@
+﻿using System.IO;
+using Kinvey;
+using Newtonsoft.Json;
+#if __ANDROID__
+using Android.App;
+using Android.Content;
+#elif __IOS__
+using Foundation;
+using UIKit;
+#endif
+
+namespace KinveySnippets
+{
+    class CodeSnippet
+#if __ANDROID__
+        : Activity
+#elif __IOS__
+        : UIViewController
+#endif
+    {
+        protected DataStore<ToDo> todoStore;
+        protected DataStore<Book> bookStore;
+        protected DataStore<Book> dataStore;
+        protected Book book;
+        protected ToDo t;
+        protected Book entity;
+        protected string your_app_key;
+        protected string your_app_secret;
+        protected Client kinveyClient;
+        protected string username;
+        protected string password;
+        protected string myUsername;
+        protected string myPassword;
+        protected string authServiceId;
+        protected string myAppKey;
+        protected string myAppSecret;
+        protected string myProjectID;
+        protected string usernameOrEmail;
+        protected EventEntity myEvent;
+        protected DataStore<Book> books;
+        protected User myFriend1;
+        protected User myFriend2;
+        protected User myUser;
+        protected FileMetaData uploadFMD;
+        protected FileMetaData fileMetaData;
+        protected string your_file_path;
+        protected byte[] content;
+        protected string downloadStreamFilePath;
+        protected string your_file_name;
+        protected string userID;
+        protected string filePath;
+        protected string collectionName;
+
+        protected byte[] GetFileDataAsByteArray() { return null; }
+        protected Stream GetFileDataAsStream() { return null; }
+#if __ANDROID__
+        protected virtual void OnNewIntent(Intent intent) { }
+#endif
+#if __IOS__
+        public virtual bool OpenUrl(UIApplication application, NSUrl url, string sourceApplication, NSObject annotation) { return false; }
+        public virtual void RegisteredForRemoteNotifications(UIApplication application, NSData deviceToken) { }
+        public virtual void FailedToRegisterForRemoteNotifications(UIApplication application, NSError error) { }
+#endif
+    }
+
+    [JsonObject(MemberSerialization.OptIn)]
+    public class ToDo : Kinvey.Entity
+    {
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("details")]
+        public string Details { get; set; }
+
+        [JsonProperty("due_date")]
+        public string DueDate { get; set; }
+    }
+}
---
+++ devcenter-snippets-collector/Program.cs
@@ -0,0 +1,259 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace devcenter_snippets_collector
+{
+    struct Snippet
+    {
+        public string File;
+        public int Line;
+        public string Code;
+    }
+
+    class Program
+    {
+        private static IEnumerable<string> IgnorePaths => new List<string> {
+            "content/downloads",
+            "content/guide/xamarin/getting-started.md",
+            "content/guide/xamarin/datastore.md",
+            "content/guide/xamarin/users.md",
+            "content/guide/xamarin/files.md",
+            "content/guide/xamarin-v3.0/migration.md",
+            "content/guide/common/enterprise/dynamics.md",
+        };
+
+        const string snippetDeclaration = "```csharp";
+
+        private static IEnumerable<Snippet> CodeSnippets(string path)
+        {
+            foreach (var ignorePath in IgnorePaths)
+            {
+                if (path.EndsWith(ignorePath, StringComparison.Ordinal)) return new List<Snippet>();
+            }
+            var attr = File.GetAttributes(path);
+            if (attr.HasFlag(FileAttributes.Directory))
+            {
+                return Directory.GetDirectories(path)
+                                .Union(Directory.GetFiles(path))
+                                .SelectMany(x => CodeSnippets(x));
+            }
+            else
+            {
+                if (Path.GetExtension(path).Equals(".md"))
+                {
+                    var fileContent = File.ReadAllText(path);
+                    var snippets = new List<Snippet>();
+                    var offset = 0;
+                    var line = 0;
+                    while (offset != fileContent.Length)
+                    {
+                        var startIndex = fileContent.IndexOf(snippetDeclaration, offset, StringComparison.Ordinal);
+                        if (startIndex == -1)
+                        {
+                            offset = fileContent.Length;
+                            continue;
+                        }
+                        line += fileContent.Substring(offset, startIndex - offset).Count(x => x == '\n') + 1;
+                        var endIndex = fileContent.IndexOf("```", startIndex + 1, StringComparison.Ordinal);
+                        if (endIndex == -1)
+                        {
+                            offset = fileContent.Length;
+                            continue;
+                        }
+                        startIndex += snippetDeclaration.Length;
+                        var code = fileContent.Substring(startIndex, endIndex - startIndex).Trim();
+                        snippets.Add(new Snippet() {
+                            File = path,
+                            Line = line,
+                            Code = code
+                        });
+                        offset = endIndex;
+                    }
+                    return snippets;
+                }
+            }
+            return new List<Snippet>();
+        }
+        static void Main(string[] args)
+        {
+            if (args.Length != 2)
+            {
+                throw new ArgumentException("Usage: devcenter-snippets-collector <devcenter path> <output cs file path>");
+            }
+            var devCenterPath = args[0].Replace("~", Environment.GetFolderPath(Environment.SpecialFolder.Personal));
+            var csFile = args[1].Replace("~", Environment.GetFolderPath(Environment.SpecialFolder.Personal));
+            if (!Directory.Exists(devCenterPath))
+            {
+                throw new ArgumentException($"DevCenter Path {args[0]} does not exists");
+            }
+            var codeSnippets = CodeSnippets(devCenterPath);
+            var usings = new List<string>();
+            var assemblies = new List<List<string>>();
+            var output = codeSnippets.Select((snippet, index) =>
+            {
+                var file = snippet.File.Substring(devCenterPath.Length + 1);
+                var linesOfCode = snippet.Code.Split("\n");
+                var snippetLines = new List<string>
+                {
+                    "",
+                    $"// {file}:{snippet.Line}"
+                };
+                usings.AddRange(linesOfCode.Where(x => x.StartsWith("using ") && !x.StartsWith("using (")).Select(x => $"{x} // {file}:{snippet.Line}"));
+                linesOfCode = linesOfCode.Where(x => {
+                    return (
+                            !x.StartsWith("using ") &&
+                            !x.StartsWith("nuget") &&
+                            !x.StartsWith("<") &&
+                            x.Trim().Length > 0
+                        ) ||
+                        x.StartsWith("using (");
+                }).ToArray();
+                var isIOS = snippet.Code.Contains("UIApplication") ||
+                            snippet.Code.Contains("NSFileManager") ||
+                            snippet.Code.Contains(".RegisterForToken()") ||
+                            snippet.Code.Contains(".DisablePush()");
+                var isAndroid = snippet.Code.Contains("Intent intent") ||
+                                snippet.Code.Contains("new Intent") ||
+                                snippet.Code.Contains("KinveyGCMService") ||
+                                snippet.Code.Contains("Android.App.Application.Context");
+                if (isIOS)
+                {
+                    snippetLines.Add("#if __IOS__");
+                }
+                else if (isAndroid)
+                {
+                    snippetLines.Add("#if __ANDROID__");
+                }
+                if (linesOfCode.Where(x => x.StartsWith("[assembly: ")).Count() > 0)
+                {
+                    snippetLines.AddRange(linesOfCode);
+                    assemblies.Add(snippetLines);
+                    if (isIOS || isAndroid)
+                    {
+                        snippetLines.Add("#endif");
+                    }
+                    return new List<string>();
+                }
+                if (linesOfCode.Where(x => {
+                    return (x.StartsWith("public ") && !x.StartsWith("public override ") && !x.StartsWith("public string ")) ||
+                        (x.StartsWith("protected ") && !x.StartsWith("protected override ")) ||
+                        (x.StartsWith("private ") && !x.StartsWith("private override ")) ||
+                        x.StartsWith("[");
+                }).Count() > 0)
+                {
+                    snippetLines.AddRange(linesOfCode);
+                    if (isIOS || isAndroid)
+                    {
+                        snippetLines.Add("#endif");
+                    }
+                    return snippetLines;
+                }
+                snippetLines.AddRange(new string[] {
+                    $"class CodeSnippet{index + 1} : CodeSnippet",
+                    "{",
+                });
+                if (linesOfCode.Where(x => {
+                    return x.StartsWith("public override ") ||
+                        x.StartsWith("protected override ") ||
+                        x.StartsWith("private override ") ||
+                        x.StartsWith("public string ");
+                }).Count() == 0)
+                {
+                    snippetLines.AddRange(new string[] {
+                        $"    async Task Snippet{index + 1}()",
+                        "    {",
+                    });
+                    snippetLines.AddRange(linesOfCode.Select(x => $"        {x}"));
+                    snippetLines.AddRange(new string[] {
+                        "        ",
+                        "        // *******************************************",
+                        "        await new Task(() => Console.WriteLine(\"\"));",
+                        "    }",
+                    });
+                }
+                else
+                {
+                    snippetLines.AddRange(linesOfCode.Select(x => $"    {x}"));
+                }
+                snippetLines.AddRange(new string[] {
+                    "}",
+                });
+                if (isIOS || isAndroid)
+                {
+                    snippetLines.Add("#endif");
+                }
+                return snippetLines;
+            }).ToArray();
+            var lines = new List<string> {
+                "using System;",
+                "using System.IO;",
+                "using System.Linq;",
+                "using System.Collections.Generic;",
+                "using System.Threading.Tasks;",
+                "using Kinvey;",
+                "using Newtonsoft.Json;",
+                "using Newtonsoft.Json.Linq;",
+                "#if __ANDROID__",
+                "    using Android.App;",
+                "    using Android.Content;",
+                "    using Android.Support.V4.App;",
+                "    using KinveySnippets.Droid;",
+                "#elif __IOS__",
+                "    using Foundation;",
+                "    using UIKit;",
+                "    using KinveySnippets.iOS;",
+                "#endif",
+                "",
+                "#pragma warning disable CS0105 // Using directive appeared previously in this namespace",
+                "#pragma warning disable CS0168 // Variable is declared but never used",
+                "#pragma warning disable RECS0022 // A catch clause that catches System.Exception and has an empty body",
+                "#pragma warning disable RECS0117 // Local variable has the same name as a member and hides it",
+            };
+            lines.AddRange(usings.Select(x => {
+                if (x.Contains("using Foundation;"))
+                {
+                    return string.Join("\n", new string[] {
+                        "#if __IOS__",
+                        x,
+                        "#endif",
+                    });
+                }
+                else if (x.Contains("using Android."))
+                {
+                    return string.Join("\n", new string[] {
+                        "#if __ANDROID__",
+                        x,
+                        "#endif",
+                    });
+                }
+                return x;
+            }));
+            lines.Add("");
+            foreach (var assembly in assemblies)
+            {
+                lines.AddRange(assembly.Where((x, i) => i == 1 || x.StartsWith("[assembly: ")));
+            }
+            foreach (var assembly in assemblies)
+            {
+                lines.AddRange(assembly.Where(x => !x.StartsWith("[assembly: ")));
+            }
+            lines.AddRange(new string[] {
+                "",
+                "namespace KinveySnippets",
+                "{",
+            });
+            lines.AddRange(output.SelectMany(x => x).Select(x => $"    {x}"));
+            lines.AddRange(new string[] {
+                "}",
+                "#pragma warning restore CS0105 // Using directive appeared previously in this namespace",
+                "#pragma warning restore CS0168 // Variable is declared but never used",
+                "#pragma warning restore RECS0022 // A catch clause that catches System.Exception and has an empty body",
+                "#pragma warning restore RECS0117 // Local variable has the same name as a member and hides it",
+            });
+            File.WriteAllLines(csFile, lines);
+            Console.WriteLine($"{codeSnippets.Count()} snippets");
+        }
+    }
+}
